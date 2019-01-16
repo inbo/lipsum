835ba47c7bc9ce7793647709b9cd9d38747874b1
ThierryO <thierry.onkelinx@inbo.be>
2019-01-16 17:22:18 +0100

    paragraph() gains a "words" argument

--- R/paragraph.R
+++ R/paragraph.R
@@ -1,6 +1,7 @@
 #' return lipsum paragraphs
 #' @param id the id of the paragraph
 #' @param n the number of random paragraphs. Only used when `id` is not given.
+#' @param words an optional prefered number of words. Only used when `ìd` is not given.
 #' @param verbose print the paragraphs
 #' @return an invisible vector of paragraphs
 #' @export
@@ -12,14 +13,25 @@
 #' paragraph(c(7, 1))
 #' z <- paragraph(1, verbose = FALSE)
 #' cat(z)
-paragraph <- function(id, n = 1, verbose = TRUE) {
+#' paragraph(words = 10, n = 2)
+paragraph <- function(id, words, n = 1, verbose = TRUE) {
   if (missing(id)) {
-    return(
-      paragraph(
-        id = sample(max(lipsum$paragraph), size = n),
-        verbose = verbose
+    if (missing(words)) {
+      id <- sample(max(lipsum$paragraph), size = n)
+    } else {
+      stopifnot(
+        is.numeric(words),
+        length(words) == 1
       )
-    )
+      available <- aggregate(words ~ paragraph, lipsum, sum)
+      id <- sample(
+        available$paragraph,
+        size = n,
+        replace = FALSE,
+        prob = 1 / (1 + abs(available$words - words) ^ 2)
+      )
+    }
+    return(paragraph(id = id, verbose = verbose))
   }
   stopifnot(
     is.numeric(id),
